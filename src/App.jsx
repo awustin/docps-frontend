0b73e2064296f56401581a4410e54ba1b245cced
Agustin <agustin94garcia@gmail.com>
2021-04-14 23:31:03 -0300

    Connecting to db

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { hot } from 'react-hot-loader';
 import React from 'react';
+import Users from './users';
 import './App.css';
 
 const message = 'Welcome to docps-frontend this is agus';
 const App = () => (
   <div className="App">
     <h1>{message}</h1>
+    <Users />
   </div>
 );
 
---
+++ src/users.jsx
@@ -0,0 +1,44 @@
+import { hot } from 'react-hot-loader';
+import React from 'react';
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    var initialState = {
+      usersList: '',
+      color: 'turquoise'
+    };
+    this.state = initialState;
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/fetch')
+      .then((response) => response.json())
+      .then((data) => this.buildUsersList(data)); 
+  }
+
+  buildUsersList(data){
+    var array = [];
+    if(data.length > 0){
+      data.forEach(element => {
+        var item = <h3>{element.nombre} {element.apellido}: {element.fecha_alta}</h3>
+        array.push(item);
+      });
+      this.setState({usersList: array});
+    }
+    else {
+      this.setState({usersList: 'No data'});
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: this.state.color }}>These are the users: </h1>
+        <h3>{this.state.usersList}</h3>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(Users);
