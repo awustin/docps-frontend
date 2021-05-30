0470505671c23240ebe3946a3667d40f98052087
Alejogb1 <52103963+Alejogb1@users.noreply.github.com>
2021-05-30 18:52:14 -0300

    DOCPS-11: Hacer Routes de executions y reports

--- src/reports/reportsMain.jsx
+++ src/reports/reportsMain.jsx
@@ -1,14 +1,26 @@
 import { hot } from 'react-hot-loader';
 import React from 'react';
+import {Switch, Route} from "react-router-dom"
 import AppLayout from '../AppLayout';
 
 class ReportsMain extends React.Component {
     render() {
         return(
             <AppLayout>
-                <div>
-                    Reportes
-                </div>
+                <Switch>
+                    <Route path="/reports/create" render={() => (
+                        <div>reports/create</div>)}
+                    />
+                    <Route path="/reports/search" render={() => (
+                        <div>reports/search</div>)}
+                    />
+                    <Route path="/reports/:id" render={() => (
+                        <div>reports/:id</div>)}
+                    />
+                    <Route path="/reports" render={() => (
+                        <div> Not found :( </div>)}
+                    />
+                </Switch>
             </AppLayout>
         );
     }
