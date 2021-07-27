bc787197163d1fb6cd656b25b8841207fec3b9b8
Agustin <agustin94garcia@gmail.com>
2021-07-26 23:07:45 -0300

    DOCPS-39: Group create

--- src/groups/groupCreateForm.jsx
+++ src/groups/groupCreateForm.jsx
@@ -13,7 +13,9 @@ import {
     Alert,
     message,
     Transfer,
-    Checkbox
+    Checkbox,
+		Select,
+		Tag
 } from 'antd';
 import {
     ExclamationCircleOutlined,
@@ -57,28 +59,54 @@ class GroupCreateForm extends React.Component {
     }
 
     handleSubmit(values) {
-        //Validar nombre único
-        /*
-				this.setState({ 
-					showMessageModal: true, 
-					message: {
-						title:'El nombre de grupo ya está en uso',
-						description:'Debe ingresar otro nombre de grupo',
-						type:'validate'
-					}
-				})
-				*/      
-        //Query para hacer el insert del grupo
-        this.setState({ 
-					success: true,
-					showMessageModal: true, 
-					message: {
-						title:'Grupo creado',
-						description:'El grupo fue creado con éxito',
-						type:'success'
-					}
-				})
+				if(!this.validAdmins(values))
+				{
+					this.setState({ 
+						showMessageModal: true, 
+						message: {
+							title:'Entrada inválida',
+							description:'Todos los administradores deben ser miembros del grupo.',
+							type:'validate'
+						}
+					})					
+				}
+				else
+				{
+					//Validar nombre único
+					/*
+					this.setState({ 
+						showMessageModal: true, 
+						message: {
+							title:'El nombre de grupo ya está en uso',
+							description:'Debe ingresar otro nombre de grupo',
+							type:'validate'
+						}
+					})
+					*/      
+					//Query para hacer el insert del grupo
+					this.setState({ 
+						success: true,
+						showMessageModal: true, 
+						message: {
+							title:'Grupo creado',
+							description:'El grupo fue creado con éxito',
+							type:'success'
+						}
+					})
+				}
     }
+		
+		validAdmins(values) {
+			//Validar q todos los admins sean members
+			if(!values.adminMembers)
+				return true
+			else
+				return values.adminMembers.every(
+					(admin) => {
+						return values.members.includes(admin)
+					}
+				)		
+		}
 		
 		closeMessageModal() {
 			const { success } = this.state
@@ -127,6 +155,9 @@ class GroupCreateForm extends React.Component {
 								>
 										<Input/>
 								</Form.Item>
+								<Col offset={5} span={14} style={{ marginBlockEnd: "1%" }}>
+									<Text type="secondary">Seleccione uno o más usuarios del panel izquierdo y agréguelos al panel derecho para añadir como miembro.</Text>
+								</Col>
 								<Form.Item 
 										label="Miembros"
 										name="members"
@@ -137,7 +168,7 @@ class GroupCreateForm extends React.Component {
 												titles={['Usuarios', 'Miembros']}
 												render={(item) => 
 														<>
-																<div style={{fontSize:"85%"}}>{item.completeName}</div>
+																<div style={{fontSize:"100%"}}>{item.completeName}</div>
 														</>
 												}
 												showSearch
@@ -149,29 +180,20 @@ class GroupCreateForm extends React.Component {
 										>
 										</Transfer>
 								</Form.Item>
-								<Form.List 
-										name="groupMembers"
+								<Form.Item
+									label="Administradores"
+									name="adminMembers"
 								>
-								{()=> (
-										<>
-												{
-														(targetUsers === undefined) ? [] : targetUsers.map((item)=>(
-																		<Form.Item
-																				label="Administrador/a de grupo"
-																				name={[item,'isAdmin']}
-																				valuePropName="checked"
-																				initialValue={false}
-																				key={item}
-																		>
-																				<Checkbox>{this.getUserCompleteName(item)}</Checkbox>
-																		</Form.Item>
-																		)
-																)
-												}
-										</>
-										)
-								}
-								</Form.List>                
+									<Select
+										mode="multiple"
+										showArrow
+										options={
+											(targetUsers === undefined) ? [] 
+											: 
+											targetUsers.map((item)=>({value: item, label:this.getUserCompleteName(item), key:item}))
+										}
+									/>
+								</Form.Item>              
 								<Form.Item {...tailLayout}>
 										<Button type="primary" htmlType="submit">Crear</Button>
 										<Button htmlType="button" onClick={this.resetForm} style={{ margin: '0 8px' }}>Limpiar</Button>
