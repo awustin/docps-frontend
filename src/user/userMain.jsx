1b3cf4c2797ccaa0f3e8658f83ec8b6ac97ee885
Garcia <agustin.garcia@sovos.com>
2021-11-07 17:35:45 -0300

    User main component

--- src/user/userMain.jsx
+++ src/user/userMain.jsx
@@ -1,33 +1,60 @@
-import { hot } from 'react-hot-loader';
+import {
+    LeftCircleOutlined
+} from '@ant-design/icons';
+import {
+    Breadcrumb, Col, Divider, Row, Tooltip, Typography
+} from 'antd';
 import React from 'react';
-import { Route,Switch,Redirect } from 'react-router-dom';
+import { withRouter } from "react-router";
+import { Redirect, Route, Switch } from 'react-router-dom';
 import AppLayout from '../AppLayout';
 import UserHome from './userHome';
-import UserManagement from './userManagement';
+import UserList from './userList';
+
+const { Title } = Typography;
 
 class UserMain extends React.Component {
     render() {
         const { user } = this.props
-        return(
+        return (
             <AppLayout user={user}>
                 <Switch>
                     <Route exact path="/user/home" render={() => (
-												<UserHome
-													user={user}
-												/>)}
+                        <UserHome
+                            user={user}
+                        />)}
                     />
                     <Route exact path="/user/admin" render={() => (
                         (user.isAdmin) ? (
-                            <UserManagement
-                                user={user}
-                            />
+                            <>
+                                <Breadcrumb>
+                                    <Breadcrumb.Item>{user.name}</Breadcrumb.Item>
+                                    <Breadcrumb.Item>Gestión de usuarios</Breadcrumb.Item>
+                                </Breadcrumb>
+                                <div className="navigation">
+                                    <Row>
+                                        <Col flex="1 0 25%">
+                                            <Tooltip title="Atrás">
+                                                <LeftCircleOutlined style={{ fontSize: "200%" }} onClick={() => { this.props.history.goBack() }} />
+                                            </Tooltip>
+                                        </Col>
+                                    </Row>
+                                </div>
+                                <div className="container">
+                                    <Title level={2} style={{ marginBlockEnd: "0px" }}>Gestión de usuarios</Title>
+                                    <Divider />
+                                    <UserList
+                                        user={user}
+                                    />
+                                </div>
+                            </>
                         ) : (
                             <div>Ud no es administrador</div>
-                        )                        
-                        )}
+                        )
+                    )}
                     />
                     <Route path="/user" render={() => (
-											<Redirect to="/user/home"/>)}
+                        <Redirect to="/user/home" />)}
                     />
                 </Switch>
             </AppLayout>
@@ -35,4 +62,4 @@ class UserMain extends React.Component {
     }
 }
 
-export default hot(module)(UserMain);
+export default withRouter(UserMain);
